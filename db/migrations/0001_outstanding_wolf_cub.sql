0bc6f171c01a117698d4f82892bc87b703610551
Mettle X <56449790+mettlex@users.noreply.github.com>
2024-04-04 02:03:44 +0600

    Add indexes to 'literally-chained-words' and 'literally-wc-unlimited' tables

---
+++ db/migrations/0001_outstanding_wolf_cub.sql
@@ -0,0 +1,2 @@
+CREATE INDEX `chain_id_index` ON `literally-chained-words` (`chain_id`);--> statement-breakpoint
+CREATE INDEX `discord_channel_id_index` ON `literally-wc-unlimited` (`discord_channel_id`);
